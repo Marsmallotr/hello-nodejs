01ebc779e8ad3ce3dbce269f0dff95ac5a9a53b8
Marsmallotr <52882165+Marsmallotr@users.noreply.github.com>
2023-04-17 23:04:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('Hello 123456');
+  res.send('Hello 1234567');
 });
 app.get('/contact', function (req, res) {
   res.send('<h1>1234567890 this is from Hanoi</h1>');
